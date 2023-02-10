21b5f11c2122d0973d6f5d8d29fd6d906c1cf925
Blenderwizard <rathelotj@gmail.com>
2023-02-10 16:31:11 +0100

    Added Grace "no" main function rule to rust version

--- Grace/Rust/src/main.rs
+++ Grace/Rust/src/main.rs
@@ -1,11 +1,13 @@
 #![allow(non_snake_case)]
+#![no_main]
 
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
+#[export_name = "main"]
+fn not_main() -> std::io::Result<()> {
     let mut file = File::create("Grace_kid.rs")?;
-    let a = "#![allow(non_snake_case)]\n\nuse std::fs::File;\nuse std::io::prelude::*;\n\nfn main() -> std::io::Result<()> {\n    let mut file = File::create(\"Grace_kid.rs\")?;\n    let a = ";
+    let a = "#![allow(non_snake_case)]\n#![no_main]\n\nuse std::fs::File;\nuse std::io::prelude::*;\n\n#[export_name = \"main\"]\nfn not_main() -> std::io::Result<()> {\n    let mut file = File::create(\"Grace_kid.rs\")?;\n    let a = ";
     let c = "\n    ";
     let b = "\n    file.write_all(format!(\"{a}{a:?};{c}let c = {c:?};{c}let b = {b:?};{b}\").as_bytes())?;\n    Ok(())\n}\n";
     file.write_all(format!("{a}{a:?};{c}let c = {c:?};{c}let b = {b:?};{b}").as_bytes())?;
